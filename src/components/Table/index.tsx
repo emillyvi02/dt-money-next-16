a800ad920239a4abf2197d5f58b25ca274c8af13
Emilly Vitória <ev192700@gmail.com>
2026-03-27 13:36:02 -0300

    Add onDelete and onEdit props to Table component

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -3,8 +3,11 @@ import { formatDate, formatPrice } from "@/utils"
 
 export type TableProps = {
     data: ITransaction[]
+    onDelete: (id: string) => void
+    onEdit: (transaction: ITransaction) => void
 }
-export const Table = ({ data }: TableProps) => {
+
+export const Table = ({ data, onDelete, onEdit }: TableProps) => {
     return <>
         <table className="w-full mt-16 border-separate border-spacing-y-2">
             <thead>
@@ -13,15 +16,37 @@ export const Table = ({ data }: TableProps) => {
                    <th className="px-4 text-left text-table-header text-base font-medium">Preço</th> 
                    <th className="px-4 text-left text-table-header text-base font-medium">Categoria</th> 
                    <th className="px-4 text-left text-table-header text-base font-medium">Data</th> 
+                   <th className="px-4 text-left text-table-header text-base font-medium">Ações</th> 
                 </tr>
             </thead> 
             <tbody>
                {data.map(transaction => (
                 <tr key={transaction.id} className="h-16">
-                   <td className="px-4 py-4 whitespace-nowrap text-title bg-white rounded-l-lg">{transaction.title} </td> 
-                   <td className={`px-4 py-4 whitespace-nowrap ${transaction.type === "INCOME"? "text-income": "text-outcome"} bg-white text-right`}>{formatPrice(transaction.price)} </td> 
-                   <td className="px-4 py-4 whitespace-nowrap text-title bg-white">{transaction.category} </td>
-                   <td className="px-4 py-4 whitespace-nowrap text-title bg-white rounded-r-lg">{formatDate(transaction.data)} </td>
+                   <td className="px-4 py-4 whitespace-nowrap text-title bg-white rounded-l-lg">
+                     {transaction.title}
+                   </td> 
+
+                   <td className={px-4 py-4 whitespace-nowrap ${transaction.type === "INCOME"? "text-income": "text-outcome"} bg-white text-right}>
+                     {formatPrice(transaction.price)}
+                   </td> 
+
+                   <td className="px-4 py-4 whitespace-nowrap text-title bg-white">
+                     {transaction.category}
+                   </td>
+
+                   <td className="px-4 py-4 whitespace-nowrap text-title bg-white">
+                     {formatDate(transaction.data)}
+                   </td>
+
+                   <td className="px-4 py-4 whitespace-nowrap text-title bg-white rounded-r-lg">
+                     <button onClick={() => onEdit(transaction)}>
+                       Editar
+                     </button>
+
+                     <button onClick={() => onDelete(transaction.id)}>
+                       Excluir
+                     </button>
+                   </td>
                 </tr>
                ))} 
             </tbody>
